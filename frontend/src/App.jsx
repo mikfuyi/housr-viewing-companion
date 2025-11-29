4615e9a70c96e3aee6d6c8adeda9397fe33d02fc
Mxr1am <mshahidbano@gmail.com>
2025-11-29 20:25:16 +0000

    houses data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Landlord from "./pages/Landlord.jsx";
 import Client from "./pages/Client.jsx";
+import Audio from "./pages/Audio.jsx"; 
+
 
 export default function App() {
   return (
@@ -9,6 +11,7 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/landlord" element={<Landlord />} />
       <Route path="/client" element={<Client />} />
+      <Route path="/audio" element={<Audio />} /> {/* 👈 new route */}
     </Routes>
   );
 }
